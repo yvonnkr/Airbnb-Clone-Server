d85cc67dcc1d0a81ae76939d099e9ec16b55b9db
yvonnkr <yvonnkr86@gmail.com>
2024-09-17 00:26:47 +0100

    Initial commit

---
+++ src/main/java/com/yvolabs/airbnbclone/AirbnbCloneServerApplication.java
@@ -0,0 +1,13 @@
+package com.yvolabs.airbnbclone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirbnbCloneServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AirbnbCloneServerApplication.class, args);
+    }
+
+}
