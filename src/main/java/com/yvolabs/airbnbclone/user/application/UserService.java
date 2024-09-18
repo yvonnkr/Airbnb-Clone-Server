ac5de44bec3d56ea39fc45e44c9c6b57d7e4b55c
yvonnkr <yvonnkr86@gmail.com>
2024-09-18 22:08:23 +0100

    implement app security, authentication and authorization with authO

---
+++ src/main/java/com/yvolabs/airbnbclone/user/application/UserService.java
@@ -0,0 +1,86 @@
+package com.yvolabs.airbnbclone.user.application;
+
+import com.yvolabs.airbnbclone.infrastructure.config.SecurityUtils;
+import com.yvolabs.airbnbclone.user.application.dto.ReadUserDTO;
+import com.yvolabs.airbnbclone.user.domain.User;
+import com.yvolabs.airbnbclone.user.mapper.UserMapper;
+import com.yvolabs.airbnbclone.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.Instant;
+import java.util.Map;
+import java.util.Optional;
+import java.util.UUID;
+
+/**
+ * @author Yvonne N
+ * @version 1.0
+ * @since 18/09/2024
+ */
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+    private static final String UPDATED_AT_KEY = "updated_at";
+    private final UserRepository userRepository;
+    private final UserMapper userMapper;
+
+    @Transactional(readOnly = true)
+    public ReadUserDTO getAuthenticatedUserFromSecurityContext() {
+        OAuth2User principal = (OAuth2User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        User user = SecurityUtils.mapOauth2AttributesToUser(principal.getAttributes());
+        return getByEmail(user.getEmail()).orElseThrow();
+    }
+
+    @Transactional(readOnly = true)
+    public Optional<ReadUserDTO> getByEmail(String email) {
+        Optional<User> oneByEmail = userRepository.findOneByEmail(email);
+        return oneByEmail.map(userMapper::readUserDTOToUser);
+    }
+
+    public void syncWithIdp(OAuth2User oAuth2User, boolean forceResync) {
+        Map<String, Object> attributes = oAuth2User.getAttributes();
+        User user = SecurityUtils.mapOauth2AttributesToUser(attributes);
+        Optional<User> existingUser = userRepository.findOneByEmail(user.getEmail());
+        if (existingUser.isPresent()) {
+            if (attributes.get(UPDATED_AT_KEY) != null) {
+                Instant lastModifiedDate = existingUser.orElseThrow().getLastModifiedDate();
+                Instant idpModifiedDate;
+                if (attributes.get(UPDATED_AT_KEY) instanceof Instant instant) {
+                    idpModifiedDate = instant;
+                } else {
+                    idpModifiedDate = Instant.ofEpochSecond((Integer) attributes.get(UPDATED_AT_KEY));
+                }
+                if (idpModifiedDate.isAfter(lastModifiedDate) || forceResync) {
+                    updateUser(user);
+                }
+            }
+        } else {
+            userRepository.saveAndFlush(user);
+        }
+    }
+
+    private void updateUser(User user) {
+        Optional<User> userToUpdateOpt = userRepository.findOneByEmail(user.getEmail());
+        if (userToUpdateOpt.isPresent()) {
+            User userToUpdate = userToUpdateOpt.get();
+            userToUpdate.setEmail(user.getEmail());
+            userToUpdate.setFirstName(user.getFirstName());
+            userToUpdate.setLastName(user.getLastName());
+            userToUpdate.setAuthorities(user.getAuthorities());
+            userToUpdate.setImageUrl(user.getImageUrl());
+            userRepository.saveAndFlush(userToUpdate);
+        }
+    }
+
+    public Optional<ReadUserDTO> getByPublicId(UUID publicId) {
+        Optional<User> oneByPublicId = userRepository.findOneByPublicId(publicId);
+        return oneByPublicId.map(userMapper::readUserDTOToUser);
+    }
+
+}
